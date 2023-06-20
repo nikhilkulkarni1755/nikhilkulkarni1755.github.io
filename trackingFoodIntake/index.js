b8a728adcb6f15a75ac653ec34603427ed62bac2
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2023-06-19 21:10:46 -0700

    [trackFood] more changes

--- trackingFoodIntake/index.js
+++ trackingFoodIntake/index.js
@@ -3,5 +3,9 @@ function trackCalories() {
 
     var date = document.getElementById("dateInput").value
 
+    var mealType = document.getElementById("mealType").value
+
     console.log(date)
+    console.log(mealType)
+    document.getElementById("totalCalories").innerHTML = date + ", " + mealType;
 }
