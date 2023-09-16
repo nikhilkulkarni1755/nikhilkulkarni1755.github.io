cc8f8ef3682eb732974c406992ec9ba1b91798d5
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2023-09-16 17:17:15 -0400

    [NodeJS][Angular] Updating Github Pages to use angular

--- goalTracking/index.js
+++ goalTracking/index.js
@@ -1,36 +1,49 @@
-function calculateDays() {
-  var d = new Date()
-  var year = d.getFullYear()
-  var month = d.getMonth() + 1
+import React, {StrictMode} from "react"
+import {createRoot} from "react-dom/client"
+import "./index.css"
 
-  var days = 0
-  switch(month) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-        days += 31
-        break
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        days += 30
-        break
-    case 2:
-        if(year % 4 == 0) {
-            days += 29
-        }
-        else {
-            days += 28
-        }
-        break
-  }
+import App from "./App"
 
-  console.log(year + ", " + month + ", " + days)
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
+
+// function calculateDays() {
+//   var d = new Date()
+//   var year = d.getFullYear()
+//   var month = d.getMonth() + 1
+
+//   var days = 0
+//   switch(month) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         days += 31
+//         break
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         days += 30
+//         break
+//     case 2:
+//         if(year % 4 == 0) {
+//             days += 29
+//         }
+//         else {
+//             days += 28
+//         }
+//         break
+//   }
+
+//   console.log(year + ", " + month + ", " + days)
 
   
 //   clone.innerHTML = window.localStorage.getItem(0 + '')
@@ -43,4 +56,4 @@ function calculateDays() {
 
 
 
-}
+// }
